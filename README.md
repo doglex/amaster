34e03ef030e17942d4936ecb4c5e50bda5eb2f20
dogless <2295841729@qq.com>
2021-02-16 17:26:03 +0800

    install electron

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # amaster
 安卓 电脑助手 ： 同步文件
+
+
+# develop 
+```
+npm install
+echo electron.exe > node_modules\electron\path.txt
+node node_modules\electron\install.js
+npm start
+```
---
+++ main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow, Notification, BrowserView , dialog, remote, ipcMain, webview} = require('electron')
+app.setAppUserModelId('aMaster')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 800,
+        frame: false,
+        icon:"lib/fav.ico",
+        backgroundColor: "black",
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag:true,
+            enableRemoteModule:true,
+            contextIsolation: false,
+            webSecurity: true
+        }
+    })
+    win.loadFile('main.html')
+    // win.loadFile('index.html')
+    win.openDevTools();
+
+}
+
+ipcMain.on('e111', (event, arg) => {
+    console.log(arg) // prints "ping"
+    event.returnValue = 'pong'
+})
+
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
+
